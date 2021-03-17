e180241face91be9f2ccf4b65a25e1b5d7b7ab2d
Aleksey Kliger <alklig@microsoft.com>
2021-03-17 13:27:13 -0400

    Add -outputSummary:results.json option for script runner
    Writes the names of the generated changes to a file

---
+++ src/Microsoft.DotNet.HotReload.Utils.Generator/Microsoft.DotNet.HotReload.Utils.Generator/OutputSummary/OutputSummary.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Text.Json.Serialization;
+
+
+namespace Microsoft.DotNet.HotReload.Utils.Generator.OutputSummary
+{
+    public class OutputSummary {
+        [JsonPropertyName("deltas")]
+        public Delta[] Deltas {get; }
+
+        public OutputSummary(Delta[] deltas) {
+            Deltas = deltas;
+        }
+    }
+
+    public class Delta {
+        [JsonPropertyName("assembly")]
+        public string Assembly {get; }
+        [JsonPropertyName("metadata")]
+        public string Metadata {get; }
+        [JsonPropertyName("il")]
+        public string IL {get; }
+        [JsonPropertyName("pdb")]
+        public string Pdb {get; }
+
+        public Delta (string assembly, string metadata, string il, string pdb) {
+            Assembly = assembly;
+            Metadata = metadata;
+            IL = il;
+            Pdb = pdb;
+        }
+    }
+}
