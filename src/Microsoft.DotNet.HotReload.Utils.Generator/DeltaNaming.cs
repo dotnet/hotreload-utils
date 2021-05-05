075f8ea5771c103bf4a31519e57fd85937b2d591
Aleksey Kliger (λgeek) <alklig@microsoft.com>
2021-05-04 23:01:29 -0400

    Cleanup code organization and style (#15)
    No functional changes
    
    * update .editorconfig
    
    * Remove one redundant subdirectory
    
    * Add copyright comments; remove unused usings

--- src/Microsoft.DotNet.HotReload.Utils.Generator/DeltaNaming.cs
+++ src/Microsoft.DotNet.HotReload.Utils.Generator/DeltaNaming.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 namespace Microsoft.DotNet.HotReload.Utils.Generator
 {
     /// Properties describing names of the artifacts for a given revision of a baseline assembly
