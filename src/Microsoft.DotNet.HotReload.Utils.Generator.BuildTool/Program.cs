cc115313d2711f7f518f8aee349f0706a688497e
Aleksey Kliger <alklig@microsoft.com>
2021-05-04 20:15:13 -0400

    Review feedback

--- src/Microsoft.DotNet.HotReload.Utils.Generator.BuildTool/Program.cs
+++ src/Microsoft.DotNet.HotReload.Utils.Generator.BuildTool/Program.cs
@@ -1,2 +1,4 @@
-﻿
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 Microsoft.DotNet.HotReload.Utils.Generator.Frontend.Frontend.Main(args);
