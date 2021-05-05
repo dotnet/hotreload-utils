9e1f97395710e5477c1e4dbccbb2084b1c0bb5a4
Aleksey Kliger <alklig@microsoft.com>
2021-05-04 22:31:55 -0400

    Add copyright comments; remove unused usings

--- src/Microsoft.DotNet.HotReload.Utils.Generator.Frontend/Frontend.cs
+++ src/Microsoft.DotNet.HotReload.Utils.Generator.Frontend/Frontend.cs
@@ -4,10 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.DotNet.HotReload.Utils.Generator.Frontend
--- src/Microsoft.DotNet.HotReload.Utils.Generator/DocResolver.cs
+++ src/Microsoft.DotNet.HotReload.Utils.Generator/DocResolver.cs
@@ -1,11 +1,10 @@
-using System;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using System.IO;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
 
 using Microsoft.CodeAnalysis;
 
