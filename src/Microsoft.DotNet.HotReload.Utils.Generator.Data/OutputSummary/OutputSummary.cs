ef521d255506071024e81738cb77f4511d6dafa2
Aleksey Kliger <aleksey@lambdageek.org>
2021-11-24 10:14:09 -0500

    A few more namespace statements

--- src/Microsoft.DotNet.HotReload.Utils.Generator.Data/OutputSummary/OutputSummary.cs
+++ src/Microsoft.DotNet.HotReload.Utils.Generator.Data/OutputSummary/OutputSummary.cs
@@ -4,40 +4,39 @@
 using System.Text.Json.Serialization;
 
 
-namespace Microsoft.DotNet.HotReload.Utils.Generator.OutputSummary
-{
-    public class OutputSummary {
-        [JsonPropertyName("deltas")]
-        public Delta[]? Deltas {get; init;}
+namespace Microsoft.DotNet.HotReload.Utils.Generator.OutputSummary;
 
-        [JsonExtensionData]
-        public System.Collections.Generic.Dictionary<string, object>? Extra {get; init;}
+public class OutputSummary {
+    [JsonPropertyName("deltas")]
+    public Delta[]? Deltas {get; init;}
 
-        [JsonConstructor]
-        public OutputSummary(Delta[]? deltas) {
-            Deltas = deltas;
-        }
+    [JsonExtensionData]
+    public System.Collections.Generic.Dictionary<string, object>? Extra {get; init;}
+
+    [JsonConstructor]
+    public OutputSummary(Delta[]? deltas) {
+        Deltas = deltas;
+    }
+}
+
+public class Delta {
+    [JsonPropertyName("assembly")]
+    public string? Assembly {get; init;}
+    [JsonPropertyName("metadata")]
+    public string? Metadata {get; init;}
+    [JsonPropertyName("il")]
+    public string? IL {get; init;}
+    [JsonPropertyName("pdb")]
+    public string? Pdb {get; init;}
+
+    [JsonConstructor]
+    public Delta (string? assembly, string? metadata, string? il, string? pdb) {
+        Assembly = assembly;
+        Metadata = metadata;
+        IL = il;
+        Pdb = pdb;
     }
 
-    public class Delta {
-        [JsonPropertyName("assembly")]
-        public string? Assembly {get; init;}
-        [JsonPropertyName("metadata")]
-        public string? Metadata {get; init;}
-        [JsonPropertyName("il")]
-        public string? IL {get; init;}
-        [JsonPropertyName("pdb")]
-        public string? Pdb {get; init;}
-
-        [JsonConstructor]
-        public Delta (string? assembly, string? metadata, string? il, string? pdb) {
-            Assembly = assembly;
-            Metadata = metadata;
-            IL = il;
-            Pdb = pdb;
-        }
-
-        [JsonExtensionData]
-        public System.Collections.Generic.Dictionary<string, object>? Extra {get; set;}
-    }
+    [JsonExtensionData]
+    public System.Collections.Generic.Dictionary<string, object>? Extra {get; set;}
 }
--- src/Microsoft.DotNet.HotReload.Utils.Generator.Data/Script/Json/Script.cs
+++ src/Microsoft.DotNet.HotReload.Utils.Generator.Data/Script/Json/Script.cs
@@ -1,38 +1,30 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using System.Diagnostics.CodeAnalysis;
 
-namespace Microsoft.DotNet.HotReload.Utils.Generator.Script.Json {
-    public class Script {
+namespace Microsoft.DotNet.HotReload.Utils.Generator.Script.Json;
+public class Script {
 
-        [JsonConverter(typeof(ScriptCapabilitiesConverter))]
-        public string? Capabilities {get; init;}
-         public Change[]? Changes {get; init;}
+    [JsonConverter(typeof(ScriptCapabilitiesConverter))]
+    public string? Capabilities {get; init;}
+        public Change[]? Changes {get; init;}
 
-        [System.Text.Json.Serialization.JsonConstructor]
-        public Script (string? capabilities, Change[]? changes) {
-            Capabilities = capabilities;
-            Changes = changes;
-        }
-
+    [System.Text.Json.Serialization.JsonConstructor]
+    public Script (string? capabilities, Change[]? changes) {
+        Capabilities = capabilities;
+        Changes = changes;
     }
 
-    public class Change {
-        public string Document {get; init;}
-        public string Update {get; init;}
+}
 
-        [System.Text.Json.Serialization.JsonConstructor]
-        public Change (string document, string update) {
-            Document = document;
-            Update = update;
-        }
+public class Change {
+    public string Document {get; init;}
+    public string Update {get; init;}
+
+    [System.Text.Json.Serialization.JsonConstructor]
+    public Change (string document, string update) {
+        Document = document;
+        Update = update;
     }
 }
